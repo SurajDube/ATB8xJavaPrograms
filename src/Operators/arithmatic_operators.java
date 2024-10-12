766367dfd7619ce4d87b80d0f708931e0d96ec69
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-10-12 18:32:01 +0530

    Add all the files and folder.

--- src/Operators/arithmatic_operators.java
+++ src/Operators/arithmatic_operators.java
@@ -1,10 +1,13 @@
 package Operators;
-
+import java.io.*;
 public class arithmatic_operators {
     public static void main(String[] args) {
-        int a = 4;
-        int b = 5;
-        int c = a+b;
-        System.out.println(c);
+        int a = 10;
+        int b = 3;
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
     }
 }
