061dccaf0c48c96ca597019326165a11b58d241c
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-10-12 08:10:43 +0530

    Add all the files and folder.

---
+++ src/DataTypePractice/primitiveDataType/booleanEx2.java
@@ -0,0 +1,16 @@
+package DataTypePractice.primitiveDataType;
+
+public class booleanEx2 {
+
+        public static void main(String args[]) {
+            boolean var1 = true;
+            if (var1 == true) //checks if the value is true or false
+            {
+                System.out.println("Boolean value is True");
+            }
+            else
+            {
+                System.out.println("Boolean value is False");
+            }
+        }
+    }
---
+++ src/DataTypePractice/primitiveDataType/booleanPractice.java
@@ -0,0 +1,15 @@
+package DataTypePractice.primitiveDataType;
+
+public class booleanPractice {
+    public static void main(String args[]) {
+        boolean var1 = true;
+        if (var1 == true) //checks if the value is true or false
+        {
+            System.out.println("Boolean value is True");
+        }
+        else
+        {
+            System.out.println("Boolean value is False");
+        }
+    }
+}
