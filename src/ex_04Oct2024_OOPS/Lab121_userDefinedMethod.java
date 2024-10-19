5678389b67dfe753f08cd1b589198c681adc1012
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-10-19 20:56:54 +0530

    Add all the files and folder till 02 October 2024 with all Function.

--- src/ex_04Oct2024_OOPS/Lab121_userDefinedMethod.java
+++ src/ex_04Oct2024_OOPS/Lab121_userDefinedMethod.java
@@ -7,22 +7,17 @@ public class Lab121_userDefinedMethod {
 //creating Scanner class object 1st part
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter the number: ");
-
         //reading value from user   3rd part
         int num = scan.nextInt();
 //method calling    4th part
         findEvenOdd(num);
-
     }
 
     //user defined method 2nd part
     public static void findEvenOdd(int num) {
 //method body
-        if (num % 2 == 0)
+        if (num % 2 == 0){
             System.out.println(num + " is even");
-        else
-            System.out.println(num + " is odd");
+        } else System.out.println(num + " is odd");
     }
-
 }
-
