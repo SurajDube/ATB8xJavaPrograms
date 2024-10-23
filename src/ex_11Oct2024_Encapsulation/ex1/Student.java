9200f8e3a172003d774756c7f53ab941b40e80f4
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-10-23 20:19:20 +0530

    Add all the files till 11 October 2024 but not complete task (11Oct)

---
+++ src/ex_11Oct2024_Encapsulation/ex1/Student.java
@@ -0,0 +1,15 @@
+package ex_11Oct2024_Encapsulation.ex1;
+
+public class Student {
+    //private data member
+    private String name;
+
+    //getters
+    public String getName() {
+        return name;
+    }
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+}
