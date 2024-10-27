6070438152cf517e1bd6efffdf6582e4f6f4d8d9
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-10-27 13:49:36 +0530

    Add all the files till 16 - Oct - 2024.

---
+++ src/ex_16Oct2024_Enum/ex4/Lab141.java
@@ -0,0 +1,10 @@
+package ex_16Oct2024_Enum.ex4;
+
+public class Lab141 {
+    public static void main(String[] args) {
+        System.out.println(apiConstantEndpoints.BASE_URL.getName());
+        System.out.println(apiConstantEndpoints.GMAIL_URL.getName());
+        System.out.println(apiConstantEndpoints.BOOKS_URL.getName());
+    }
+
+}
