740d11a4a7af035757caa81de3e45818aec2a77c
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-11-07 13:58:12 +0530

    Add all the files till 04 - Nov - 2024.

---
+++ src/ex_28Oct2024_Collection/ArrayList/Lab172List.java
@@ -0,0 +1,11 @@
+package ex_28Oct2024_Collection.ArrayList;
+
+import java.util.List;
+
+public class Lab172List {
+    public static void main(String[] args) {
+        List obj = List.of("Obj 1", "Obj 2", "Obj 3");  //static can not add
+        System.out.println(obj.size());
+
+    }
+}
