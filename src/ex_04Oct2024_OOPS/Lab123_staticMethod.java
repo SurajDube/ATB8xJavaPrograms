a3dc74f3c1d0c29c9fdc5a5345e667007b7344a1
SURAJ DUBEY <pfs.surajdubey@gmail.com>
2024-11-19 12:15:35 +0530

    Add all the files till - November - 2024 with Task.

--- src/ex_04Oct2024_OOPS/Lab123_staticMethod.java
+++ src/ex_04Oct2024_OOPS/Lab123_staticMethod.java
@@ -3,10 +3,17 @@ package ex_04Oct2024_OOPS;
 public class Lab123_staticMethod {
     public static void main(String[] args)
     {
-        show();
+        show1();
+        Lab123_staticMethod nm = new Lab123_staticMethod();
+        nm.show2();
+
     }
-    public static void show()
+    public static void show1()
     {
         System.out.println("It is an example of static method.");
+    }
+    public void show2()
+    {
+        System.out.println("It is an example of non static method.");
     }
 }
--- src/ex_04Oct_2024_Constructor/dogDC.java
+++ src/ex_04Oct_2024_Constructor/dogDC.java
@@ -1,5 +1,6 @@
 package ex_04Oct_2024_Constructor;
 public class dogDC {
+    // Constructor must have no return type
     // you can create two class.
     // one for variable or attributes
     // Second for calling method (with main and user defined method.)
